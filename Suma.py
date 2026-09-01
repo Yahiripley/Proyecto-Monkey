cb2f8b05cad1f11f399c9a7972107acdf4749c7b
Yahiripley <omar.sandova267149@potros.itson.edu.mx>
2026-09-01 11:55:46 -0700

    Calculadora de suma

---
+++ Suma.py
@@ -0,0 +1,8 @@
+print("Bienvenido a esta calculadora de suma. Por favor, ingrese dos números para sumarlos.")
+
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+suma = num1 + num2
+
+print(f"La suma de {num1} y {num2} es: {suma}")
